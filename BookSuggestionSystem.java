29d3836c0b9b81f4aacbd88539c802a09e694819
MEGHANA KOPPULA <141392061+MEGHANA-KOPPULA@users.noreply.github.com>
2023-10-17 16:24:58 +0530

    Add files via upload

--- BookSuggestionSystem.java
+++ BookSuggestionSystem.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class BookSuggestionSystem {
     public static void main(String[] args) {
@@ -9,26 +8,11 @@ public class BookSuggestionSystem {
         books.put("Harry Potter and the Sorcerer's Stone", Map.of("Genre", "Fiction", "Link", "https://www.read-any-book.net/online/68"));
         // Add more books with genres and links
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Book Suggestion System!");
-        System.out.print("Enter a book genre: ");
-        String userGenre = scanner.nextLine();
-
-        boolean foundGenre = false;
-
+        System.out.println("Genres and Book Links:");
         for (String book : books.keySet()) {
             Map<String, String> bookData = books.get(book);
-            if (bookData.get("Genre").equalsIgnoreCase(userGenre)) {
-                foundGenre = true;
-                System.out.println(book + ": " + bookData.get("Link"));
-            }
+            String genre = bookData.get("Genre");
+            String link = bookData.get("Link");
+            System.out.println("Genre: " + genre + ", Book Link: " + link);
         }
-
-        if (!foundGenre) {
-            System.out.println("Sorry, no books found in the specified genre.");
-        }
-
-        scanner.close();
     }
-}
